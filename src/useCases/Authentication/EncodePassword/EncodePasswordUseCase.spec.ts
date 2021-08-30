fb0f824b494ff433446d8c29c7dcf05467cb13c2
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-08-30 20:07:11 -0300

    Implementa encriptação de senha

---
+++ src/useCases/Authentication/EncodePassword/EncodePasswordUseCase.spec.ts
@@ -0,0 +1,18 @@
+import User from '../../../entities/User';
+import EncodePasswordUseCase from './EncodePasswordUseCase';
+
+describe('Testa EnodePasswordUseCase.', () => {
+  test('Verifica se o password foi encriptado.', async () => {
+    const userData = {
+      email: 'jacl@meuapp.com',
+      password: '123456789',
+      username: 'JackSR',
+    };
+    const user = new User(userData);
+    const encodePasswordUseCase = new EncodePasswordUseCase(user);
+    await encodePasswordUseCase.execute();
+
+    expect(user.password).toMatch(/^\$2b\$/);
+    expect(user.password).not.toEqual(userData.password);
+  });
+});
--- src/useCases/User/CreateUser/CreateUserUseCase.ts
+++ src/useCases/User/CreateUser/CreateUserUseCase.ts
@@ -11,6 +11,7 @@ export default class CreateUserUseCase {
     if (userAlreadyExists) throw new Error('User already exists!!');
 
     const user = new User(data);
+
     await this.usersRepository.save(user);
   }
 }
