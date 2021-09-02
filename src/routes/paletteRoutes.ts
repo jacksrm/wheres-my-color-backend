00ce4b5c540fd4aacf40a8c20cc96365d84c943f
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-09-02 11:30:43 -0300

    Altera nome do useCase da paleta

--- src/routes/paletteRoutes.ts
+++ src/routes/paletteRoutes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import createPalette from '@useCases/Palette/CreatePalette';
-import getSinglePalette from '@useCases/Palette/GetSinglePalette';
+import getSinglePalette from '@useCases/Palette/GetPalette';
+import authenticateUser from '@useCases/Authentication/AuthenticateUser';
 
 const paletteRoutes = Router();
 
-paletteRoutes.get('/:paletteId', getSinglePalette().controller.handle());
+paletteRoutes.get(
+  '/:paletteId',
+  authenticateUser().middleware.handle(),
+  getSinglePalette().controller.handle(),
+);
+
+paletteRoutes.get('/public/:paletteId', getSinglePalette().controller.handle());
 
 paletteRoutes.post('/create', createPalette().controller.handle());
 
