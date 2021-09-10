1b23e09ad34db83533a4af7720a455e627c4ea31
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-09-10 13:00:21 -0300

    Refatora GetPublicPalette

--- src/useCases/Palette/GetPublicPalette/GetPublicPaletteController.ts
+++ src/useCases/Palette/GetPublicPalette/GetPublicPaletteController.ts
@@ -5,19 +5,17 @@ import { GetPublicPaletteUseCase } from './GetPublicPaletteUseCase';
 export class GetPublicPaletteController {
   constructor(private getPublicPaletteUseCase: GetPublicPaletteUseCase) {}
 
-  handle() {
-    return async (
-      request: Request<IGetPaletteRequestDTO>,
-      response: Response,
-    ) => {
-      try {
-        const palette = await this.getPublicPaletteUseCase.execute(
-          request.params,
-        );
-        return response.status(200).json({ palette });
-      } catch (error) {
-        return response.sendStatus(404);
-      }
-    };
-  }
+  handle = async (
+    request: Request<IGetPaletteRequestDTO>,
+    response: Response,
+  ) => {
+    try {
+      const palette = await this.getPublicPaletteUseCase.execute(
+        request.params,
+      );
+      return response.status(200).json({ palette });
+    } catch (error) {
+      return response.sendStatus(404);
+    }
+  };
 }
--- src/useCases/Palette/GetPublicPalette/index.ts
+++ src/useCases/Palette/GetPublicPalette/index.ts
@@ -1,15 +1,16 @@
 import { repositoriesModule } from '@repositories/index';
+import { IRepositoryFactory } from '@repositories/IRepositoryFactory';
 import { GetPublicPaletteController } from './GetPublicPaletteController';
 import { GetPublicPaletteUseCase } from './GetPublicPaletteUseCase';
 
 const repos = repositoriesModule();
 
-export function getPublicPaletteModule() {
-  const useCase = new GetPublicPaletteUseCase(repos.palettes);
+export function getPublicPaletteModule(repository: IRepositoryFactory = repos) {
+  const useCase = new GetPublicPaletteUseCase(repository.palettes);
   const controller = new GetPublicPaletteController(useCase);
 
   return {
     useCase: useCase.execute,
-    controller: controller.handle(),
+    controller: controller.handle,
   };
 }
