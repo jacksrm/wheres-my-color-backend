d7e8e650cc3184dd74e8edca797de259b31514a7
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-09-10 12:56:28 -0300

    Refatora UpdatePalette

--- src/useCases/Palette/UpdatePalete/index.ts
+++ src/useCases/Palette/UpdatePalete/index.ts
@@ -1,15 +1,16 @@
 import { repositoriesModule } from '@repositories/index';
+import { IRepositoryFactory } from '@repositories/IRepositoryFactory';
 import { UpdatePaletteController } from './UpdatePaletteController';
 import { UpdatePaletteUseCase } from './UpdatePaletteUseCase';
 
 const repos = repositoriesModule();
 
-export function updatePaletteModule() {
-  const useCase = new UpdatePaletteUseCase(repos.palettes);
+export function updatePaletteModule(repository: IRepositoryFactory = repos) {
+  const useCase = new UpdatePaletteUseCase(repository.palettes);
   const controller = new UpdatePaletteController(useCase);
 
   return {
     useCase: useCase.execute,
-    controller: controller.handle(),
+    controller: controller.handle,
   };
 }
--- src/useCases/User/CreateUser/CreateUserUseCase.spec.ts
+++ src/useCases/User/CreateUser/CreateUserUseCase.spec.ts
@@ -5,7 +5,6 @@ import {
   conflictUsernameUserData,
 } from '@mocks/userCollection';
 import { createUserModule } from '.';
-import { CreateUserUseCase } from './CreateUserUseCase';
 
 const repos = mockRepos();
 const createUser = createUserModule(repos);
@@ -19,18 +18,14 @@ describe('Testes de CreateUserUseCase.', () => {
   });
 
   test('testa se ao passar email conflitante, lança uma exceção com a mensagem "Email already registered!!"', async () => {
-    const useCase = new CreateUserUseCase(repos.users);
-
-    await expect(useCase.execute(conflictEmailUserData))
-      .rejects
-      .toThrow(new Error('Email already registered!!'));
+    await expect(createUser.useCase(conflictEmailUserData)).rejects.toThrow(
+      new Error('Email already registered!!'),
+    );
   });
 
   test('testa se ao passar username conflitante, lança uma exceção com a mensagem "Username already registered!!"', async () => {
-    const useCase = new CreateUserUseCase(repos.users);
-
-    await expect(useCase.execute(conflictUsernameUserData))
-      .rejects
-      .toThrow(new Error('Username already registered!!'));
+    await expect(createUser.useCase(conflictUsernameUserData)).rejects.toThrow(
+      new Error('Username already registered!!'),
+    );
   });
 });
