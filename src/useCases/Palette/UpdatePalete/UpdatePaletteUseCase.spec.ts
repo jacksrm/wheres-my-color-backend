8f96e8dda4981861b3cf70bc62b453d70a31ab89
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-11-10 19:52:39 -0300

    Implementa testes de UpdatePalette

--- src/useCases/Palette/UpdatePalete/UpdatePaletteUseCase.spec.ts
+++ src/useCases/Palette/UpdatePalete/UpdatePaletteUseCase.spec.ts
@@ -1,7 +1,13 @@
+import 'dotenv/config';
+import supertest from 'supertest';
+import { User } from '@entities/User';
 import { mockRepos } from '@mocks/index';
 import { changePaletteData } from '@mocks/paletteCollection';
 import { userCollection } from '@mocks/userCollection';
+import { connection } from '@repositories/connection';
+import { app } from 'app';
 import { updatePaletteModule } from '.';
+import { ICreatePaletteRequestDTO } from '../CreatePalette/CreatePaletteDTO';
 
 import { UpdatePaletteError } from './UpdatePaletteError';
 
@@ -32,5 +38,59 @@ describe('Testes de UpdatePalette', () => {
 });
 
 describe('Testes de Integração de UpdatePalette', () => {
+  const createUserData = {
+    username: 'JackSRSupertest',
+    email: 'jacksupertest@meuapp.com',
+    password: '1234567890',
+  };
 
+  const createPaletteData2: ICreatePaletteRequestDTO = {
+    name: 'Paleta massa',
+    isPublic: true,
+  } as ICreatePaletteRequestDTO;
+
+  const loginUserData = {
+    email: 'jacksupertest@meuapp.com',
+    password: '1234567890',
+  };
+
+  let token: string;
+
+  let user: User;
+
+  const dbConnection = connection();
+
+  beforeAll(async () => {
+    await dbConnection.start();
+  });
+
+  afterAll(async () => {
+    await dbConnection.close();
+  });
+
+  beforeEach(async () => {
+    await supertest(app).post('/v1/user/create').send(createUserData);
+
+    const response = await supertest(app)
+      .post('/v1/login')
+      .set('Content-Type', 'application/json')
+      .send({
+        email: loginUserData.email,
+        password: loginUserData.password,
+      });
+
+    token = response.body.token;
+
+    const getResponse = await supertest(app)
+      .get('/v1/user/profile')
+      .auth(token, { type: 'bearer' });
+    user = getResponse.body.user;
+    createPaletteData2.ownerId = user._id;
+  });
+
+  afterEach(async () => {
+    await supertest(app)
+      .delete('/v1/user/profile/')
+      .auth(token, { type: 'bearer' });
+  });
 });
