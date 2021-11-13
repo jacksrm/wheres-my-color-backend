a1d4b95f0c40e9ebb47c98ce434338cc473923d4
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-11-13 17:23:47 -0300

    Implementa update de cor

--- src/repositories/IColorsRepository.ts
+++ src/repositories/IColorsRepository.ts
@@ -2,4 +2,6 @@ import { Color } from '@entities/Color';
 
 export interface IColorsRepository {
   create(color: Color, paletteId: string): Promise<void>;
+  updateColor(color: Color, paletteId: string): Promise<void>;
+  findColorById(paletteId: string, colorId: string): Promise<Color | null>;
 }
